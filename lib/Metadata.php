18a831becd6cf303a33675eca322551f8baf3e5f
J. King <jking@jkingweb.ca>
2020-04-28 10:00:05 -0400

    Add an initial HTTP message handler

--- lib/Metadata.php
+++ lib/Metadata.php
@@ -7,11 +7,20 @@ declare(strict_types=1);
 namespace MensBeam\Lax;
 
 class Metadata {
-    public $url;
-    public $type;
+    /** @var bool */
     public $cached = false;
-    public $lastModified;
-    public $etag;
+    /** @var \MensBeam\Lax\Url */
+    public $url;
+    /** @var \MensBeam\Lax\MimeType */
+    public $type;
+    /** @var \MensBeam\Lax\Date */
+    public $date;
+    /** @var \MensBeam\Lax\Date */
     public $expires;
+    /** @var \MensBeam\Lax\Date */
+    public $lastModified;
+    /** @var string */
+    public $etag;
+    /** @var \DateInterval */
     public $maxAge;
 }
---
+++ lib/Parser/HTTP/Message.php
@@ -0,0 +1,94 @@
+<?php
+/** @license MIT
+ * Copyright 2018 J. King et al.
+ * See LICENSE and AUTHORS files for details */
+
+declare(strict_types=1);
+namespace MensBeam\Lax\Parser\HTTP;
+
+use MensBeam\Lax\Date;
+use MensBeam\Lax\MimeType;
+use MensBeam\Lax\Url;
+use MensBeam\Lax\Feed as FeedStruct;
+use Psr\Http\Message\MessageInterface;
+
+class Message {
+    protected const TYPE_PATTERN = '/^[A-Za-z0-9!#$%&\'*+\-\.\^_`|~]+\/[A-Za-z0-9!#$%&\'*+\-\.\^_`|~]+\s*(;.*)?$/s';
+    protected const DATE_PATTERN = '/^(?:(Mon|Tue|Wed|Thu|Fri|Sat|Sun), \d\d (?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) \d{4} \d\d:\d\d:\d\d GMT|(?:Mon|Tues|Wednes|Thurs|Fri|Satur|Sun)day, \d\d-(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)-\d\d \d\d:\d\d:\d\d GMT|(?:Mon|Tue|Wed|Thu|Fri|Sat|Sun) (?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) (?:\d\d| \d) \d\d:\d\d:\d\d \d{4})$/';
+    protected const ETAG_PATTERN = '/^.+$/';
+    protected const CCON_PATTERN = '/(?:^|,)\s*[^=,]+(?:=(?:"(?:\\"|[^"])*"[^,]*|[^,]*))?/';
+
+    /** @var \Psr\Http\Message\MessageInterface */
+    protected $msg;
+
+    public function __construct(MessageInterface $msg, string $url = null) {
+        $this->msg = $msg;
+        if (strlen($url ?? "")) {
+            $this->url = $url;
+        }
+    }
+
+    public function parse(FeedStruct $feed = null): FeedStruct {
+        $feed->meta->url = strlen($this->url ?? "") ? new Url($this->url) : null;
+        $feed->meta->type = $this->getContentType();
+        $feed->meta->date = $this->getDate();
+        $feed->meta->expires = $this->getExpires();
+        $feed->meta->lastModified = $this->getLastModified();
+        $feed->meta->etag = $this->getEtag();
+        $feed->meta->maxAge = $this->getMaxAge();
+        return $feed;
+    }
+
+    protected function parseHeader(string $name, string $pattern, bool $multi = false): ?array {
+        if ($multi) {
+            if (preg_match_all($pattern, $this->msg->getHeaderLine($name), $match)) {
+                return $match;
+            }
+        } else {
+            foreach ($this->msg->getHeader($name) as $h) {
+                if (preg_match($pattern, $h, $match)) {
+                    return $match;
+                }
+            }
+        }
+        return null;
+    }
+
+    protected function parseDate(string $name): ?Date {
+        $d = $this->parseHeader($name, self::DATE_PATTERN);
+        if ($d) {
+            return Date::createFromString($d[0]);
+        }
+        return null;
+    }
+
+    public function getContentType(): ?MimeType {
+        $t = $this->parseHeader("Content-Type", self::TYPE_PATTERN);
+        if ($t) {
+            return MimeType::parse($t[0]);
+        }
+        return null;
+    }
+
+    public function getDate(): ?Date {
+        return $this->parseDate("Date");
+    }
+
+    public function getExpires(): ?Date {
+        return $this->parseDate("Expires");
+    }
+
+    public function getLastModified(): ?Date {
+        return $this->parseDate("Last-Modified");
+    }
+
+    public function getEtag(): ?string {
+        return $this->parseHeader("ETag", self::ETAG_PATTERN);
+    }
+
+    public function getMaxAge(): ?\DateInterval {
+        $c = $this->parseHeader("Cache-Control", self::CCON_PATTERN, true);
+        var_export($c);
+        exit;
+    }
+}
